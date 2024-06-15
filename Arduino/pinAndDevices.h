920fc8ed9cb8ccb3281ef41034cf0d97085c3b4a
Quyết Thành (PTITer) <115696569+mellivora24@users.noreply.github.com>
2024-06-15 12:33:07 +0700

    Create pinAndDevices.h

---
+++ Arduino/pinAndDevices.h
@@ -0,0 +1,15 @@
+/* Devices name and pins */
+#define btn_1 4
+#define btn_2 3
+#define btn_3 2
+
+#define pir_sensor A3
+#define gas_sensor A0
+#define temp_sensor A1
+#define sound_sensor A2
+
+#define servo_pin 5
+#define light 13
+#define buzzer 12
+#define cool_fan 10
+#define kittchen_fan 11
